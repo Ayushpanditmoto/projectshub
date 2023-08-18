8d72d46199a027280cf1a0e0a3f4b2171f6987d5
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-18 19:57:00 +0530

    fix: fixing css

--- src/app/coding-contest/page.tsx
+++ src/app/coding-contest/page.tsx
@@ -1,14 +1,17 @@
 
+import CenterLayout from '../components/CenterLayout';
 import ContestsList from './../components/ContestTable';
 
 const App = () => {
   return (
+    <CenterLayout>
     <div className="container mx-auto px-4 sm:px-8">
       {/* <h1 className="text-3xl font-bold my-4">Contests</h1> */}
       <ContestsList apiUrl="https://kontests.net/api/v1/leet_code" platformName="LeetCode" />
       <ContestsList apiUrl="https://kontests.net/api/v1/code_chef" platformName="CodeChef" />
       <ContestsList apiUrl="https://kontests.net/api/v1/codeforces" platformName="Codeforces" />
     </div>
+    </CenterLayout>
   );
 };
 
