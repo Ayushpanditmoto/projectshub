6acc765270750f35506c4e8a096468a312069c17
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-19 09:32:09 +0530

    fix production

--- src/app/components/ContestTable.tsx
+++ src/app/components/ContestTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Contest } from "../types/contest";
+import { Contest } from "../../types/contest";
 
 const ContestsList = ({
   apiUrl,
