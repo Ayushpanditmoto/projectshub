a9ab7f91a7086d5c88660e445af1180928a1b18a
Karina Garces ✨ <karinadgarces@gmail.com>
2023-08-25 11:56:16 -0400

    removed projects route and updated client work

---
+++ src/app/(routes)/client-work/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function clientWork() {
+  return (
+    <>
+      <h1>Client Work</h1>
+    </>
+  )
+}
+
+export default clientWork
---
+++ src/app/(routes)/upcoming-events/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function upcomingEvents() {
+    return (
+        <>
+            <h1>Upcoming Events</h1>
+        </>
+    )
+}
+
+export default upcomingEvents
