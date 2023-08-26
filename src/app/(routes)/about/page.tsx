6f6824005c09f29a7f5d955f11d5d385a6d3f9f1
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-27 00:25:35 +0530

    feat: mdx path issues

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 import React from "react";
 import HelloWorld from "./blog.mdx";
-import CenterLayout from "@/app/components/CenterLayout";
-
 function About() {
   return (
     <>
-      {/* <CenterLayout> */}
       <h1>About Us</h1>
 
       <HelloWorld />
-      {/* </CenterLayout> */}
     </>
   );
 }
--- src/app/(routes)/blog/[slug]/page.tsx
+++ src/app/(routes)/blog/[slug]/page.tsx
@@ -1,19 +1,60 @@
-"use client";
-import React from "react";
-import CenterLayout from "@/app/components/CenterLayout";
-import { useRouter, useParams } from "next/navigation";
+import { GetStaticPaths, GetStaticProps } from "next";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote";
 
-function SingleBlog() {
-  const param = useParams();
-  console.log(param.slug);
+const BlogPost = ({
+  source,
+  frontMatter,
+}: {
+  source: any;
+  frontMatter: any;
+}) => {
   return (
-    <div>
-      <CenterLayout>
-        <h1>Single Blog</h1>
-        <h2>{param.slug}</h2>
-      </CenterLayout>
-    </div>
+    <article>
+      <h1>{frontMatter.title}</h1>
+      <p>{frontMatter.date}</p>
+      <MDXRemote {...source} />
+    </article>
   );
-}
+};
 
-export default SingleBlog;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const postsDirectory = path.join(process.cwd(), "mdx");
+  console.log(postsDirectory);
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const slugs = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf-8");
+    const { data } = matter(fileContents);
+    return data.slug;
+  });
+
+  const paths = slugs.map((slug) => ({
+    params: { slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+  const { slug } = params;
+  const postsDirectory = path.join(process.cwd(), "mdx");
+  const filePath = path.join(postsDirectory, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  const { content, data } = matter(fileContents);
+
+  return {
+    props: {
+      source: content,
+      frontMatter: data,
+    },
+  };
+};
+
+export default BlogPost;
