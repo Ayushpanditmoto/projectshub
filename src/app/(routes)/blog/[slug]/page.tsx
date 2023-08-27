fa86cee926897d472650d829a83ecbe17fc5cf4d
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-27 08:44:30 +0530

    fix vercel

--- src/app/(routes)/blog/[slug]/page.tsx
+++ src/app/(routes)/blog/[slug]/page.tsx
@@ -1,13 +1,26 @@
 import BlogPost from "../../../components/BlogPost";
 import CenterLayout from "../../../components/CenterLayout";
-import fs from "fs";
 import Markdown from "markdown-to-jsx";
+
+import fs from "fs";
+import path from "path";
 
 const getPostContent = (slug: string) => {
   const folder = "blog/";
-  const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, "utf8");
-  return content;
+  const file = path.join(process.cwd(), folder, `${slug}.md`);
+
+  try {
+    const content = fs.readFileSync(file, "utf8");
+    return content;
+  } catch (error: any) {
+    if (error.code === "ENOENT") {
+      // Handle the case where the file is not found
+      return ""; // You can return null or another suitable value
+    } else {
+      // Handle other file reading errors
+      throw error;
+    }
+  }
 };
 
 const PostPage = (props: any) => {
