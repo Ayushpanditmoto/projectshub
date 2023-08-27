e79965dc3fc289fedab42ee57832b00384e7614b
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-27 20:29:54 +0530

    fix: metadata

--- src/app/AppWrap.tsx
+++ src/app/AppWrap.tsx
@@ -1,17 +1,44 @@
+"use client";
 import { ReduxProvider } from "@/redux/provider";
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Newsletter from "./components/Newsletter";
+import { MDXProvider } from "@mdx-js/react";
 
 function AppWrap({ children }: { children: React.ReactNode }) {
   return (
-    <ReduxProvider>
-      <Header />
-      {children}
-      <Newsletter />
-      <Footer />
-    </ReduxProvider>
+    <MDXProvider
+      components={{
+        h1: (props) => <h1 style={{ color: "red" }} {...props} />,
+        h2: (props) => <h2 style={{ color: "blue" }} {...props} />,
+        h3: (props) => <h3 style={{ color: "green" }} {...props} />,
+        h4: (props) => <h4 style={{ color: "yellow" }} {...props} />,
+        h5: (props) => <h5 style={{ color: "pink" }} {...props} />,
+        h6: (props) => <h6 style={{ color: "purple" }} {...props} />,
+        p: (props) => <p style={{ color: "orange" }} {...props} />,
+        a: (props) => <a style={{ color: "brown" }} {...props} />,
+        ul: (props) => <ul style={{ color: "black" }} {...props} />,
+        ol: (props) => <ol style={{ color: "white" }} {...props} />,
+        li: (props) => <li style={{ color: "gray" }} {...props} />,
+        blockquote: (props) => (
+          <blockquote style={{ color: "cyan" }} {...props} />
+        ),
+        hr: (props) => <hr style={{ color: "magenta" }} {...props} />,
+        em: (props) => <em style={{ color: "teal" }} {...props} />,
+        strong: (props) => <strong style={{ color: "lime" }} {...props} />,
+        img: (props) => (
+          <img style={{ color: "maroon", width: "50%" }} {...props} />
+        ),
+      }}
+    >
+      <ReduxProvider>
+        <Header />
+        {children}
+        <Newsletter />
+        <Footer />
+      </ReduxProvider>
+    </MDXProvider>
   );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-"use client";
+// "use client";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { MDXProvider } from "@mdx-js/react";
 import AppWrap from "./AppWrap";
 
-const inter = Quicksand({ subsets: ["latin"] });
+const inter = Poppins({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+});
 
-// export const metadata: Metadata = {
-//   title: "ProjectsHub",
-//   description: "ProjectsHub is a blog by ProjectsHub.",
-// };
+export const metadata: Metadata = {
+  title: "ProjectsHub",
+  description: "ProjectsHub is a blog by ProjectsHub.",
+};
 
 export default function RootLayout({
   children,
@@ -20,38 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <MDXProvider
-      components={{
-        h1: (props) => <h1 style={{ color: "red" }} {...props} />,
-        h2: (props) => <h2 style={{ color: "blue" }} {...props} />,
-        h3: (props) => <h3 style={{ color: "green" }} {...props} />,
-        h4: (props) => <h4 style={{ color: "yellow" }} {...props} />,
-        h5: (props) => <h5 style={{ color: "pink" }} {...props} />,
-        h6: (props) => <h6 style={{ color: "purple" }} {...props} />,
-        p: (props) => <p style={{ color: "orange" }} {...props} />,
-        a: (props) => <a style={{ color: "brown" }} {...props} />,
-        ul: (props) => <ul style={{ color: "black" }} {...props} />,
-        ol: (props) => <ol style={{ color: "white" }} {...props} />,
-        li: (props) => <li style={{ color: "gray" }} {...props} />,
-        blockquote: (props) => (
-          <blockquote style={{ color: "cyan" }} {...props} />
-        ),
-        hr: (props) => <hr style={{ color: "magenta" }} {...props} />,
-        em: (props) => <em style={{ color: "teal" }} {...props} />,
-        strong: (props) => <strong style={{ color: "lime" }} {...props} />,
-        img: (props) => (
-          <img style={{ color: "maroon", width: "50%" }} {...props} />
-        ),
-      }}
-    >
-      <UserProvider>
-        <html lang="en" suppressHydrationWarning={true}>
-          <body className={inter.className} suppressHydrationWarning={true}>
-            <AppWrap>{children}</AppWrap>
-          </body>
-          <Analytics />
-        </html>
-      </UserProvider>
-    </MDXProvider>
+    <UserProvider>
+      <html lang="en" suppressHydrationWarning={true}>
+        <body className={inter.className} suppressHydrationWarning={true}>
+          <AppWrap>{children}</AppWrap>
+        </body>
+        <Analytics />
+      </html>
+    </UserProvider>
   );
 }
