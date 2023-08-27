95d8aa091784886b27bb551980052911923751d1
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-27 08:36:57 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -13,10 +13,6 @@ const withMDX = require("@next/mdx")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure pageExtensions to include md and mdx
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  // Optionally, add any other Next.js config below
-  reactStrictMode: true,
   images: {
     domains: [
       "localhost",
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,47 +1,50 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Card() {
   return (
-    <div className="w-full max-w-sm mx-auto">
-      <div className="bg-white shadow-md rounded-md overflow-hidden">
-        {/* Blog post image */}
-        <div className="relative h-52">
-          <Image
-            src="https://unsplash.com/photos/wX2L8L-fGeA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8Z2l0aHVifGVufDB8fHx8MTY5Mjk4MzkxNnww&force=true&w=2400" // Replace with your image URL
-            alt="Blog Post"
-            fill
-            priority
-            style={{ objectFit: "cover" }}
-          />
-        </div>
+    <Link href="/blog/first-blog-post">
+      <div className="w-full max-w-sm mx-auto">
+        <div className="bg-white shadow-md rounded-md overflow-hidden">
+          {/* Blog post image */}
+          <div className="relative h-52">
+            <Image
+              src="https://unsplash.com/photos/wX2L8L-fGeA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8Z2l0aHVifGVufDB8fHx8MTY5Mjk4MzkxNnww&force=true&w=2400" // Replace with your image URL
+              alt="Blog Post"
+              fill
+              priority
+              style={{ objectFit: "cover" }}
+            />
+          </div>
 
-        {/* Blog post content */}
-        <div className="p-4">
-          <h2 className="text-xl font-semibold text-gray-800">
-            Blog Post Title
-          </h2>
-          <p className="text-gray-600 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tristique, justo et laoreet aliquet.
-          </p>
-          <div className="mt-4 flex items-center justify-between">
-            <div className="flex items-center">
-              <Image
-                src="https://unsplash.com/photos/iEEBWgY_6lA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8dXNlcnxlbnwwfHx8fDE2OTI5ODQ5MDF8MA&force=true&w=640" // Replace with author's avatar
-                alt="Author"
-                width={24}
-                height={24}
-                className="rounded-full"
-                style={{ objectFit: "cover" }}
-              />
-              <span className="text-gray-700 ml-2">Author Name</span>
+          {/* Blog post content */}
+          <div className="p-4">
+            <h2 className="text-xl font-semibold text-gray-800">
+              Blog Post Title
+            </h2>
+            <p className="text-gray-600 mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tristique, justo et laoreet aliquet.
+            </p>
+            <div className="mt-4 flex items-center justify-between">
+              <div className="flex items-center">
+                <Image
+                  src="https://unsplash.com/photos/iEEBWgY_6lA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8dXNlcnxlbnwwfHx8fDE2OTI5ODQ5MDF8MA&force=true&w=640" // Replace with author's avatar
+                  alt="Author"
+                  width={24}
+                  height={24}
+                  className="rounded-full"
+                  style={{ objectFit: "cover" }}
+                />
+                <span className="text-gray-700 ml-2">Author Name</span>
+              </div>
+              <span className="text-gray-500">Published on: July 26, 2023</span>
             </div>
-            <span className="text-gray-500">Published on: July 26, 2023</span>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
