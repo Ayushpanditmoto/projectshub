e899958a7defca929a8fa57692044d8a7bbf3a35
OleanjiKingCode <holamilean56@gmail.com>
2023-08-28 20:26:56 +0100

    edited 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,39 @@
+import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function NotFound() {
+function Page() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="container mx-auto max-w-6xl">
+      <div className="flex flex-col items-center justify-center lg:justify-between space-y-8 lg:space-y-0 ">
+        <div className="flex items-center justify-center mx-auto">
+          <Image
+            alt="404"
+            src="/static/error.png"
+            width={430}
+            height={430}
+            className="h-90 w-90"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex-1 max-w-md lg:max-w-xl lg:text-center space-y-3 md:space-y-6">
+            <h1 className="font-bold text-xl md:text-4xl">OPPS! LOST?</h1>
+            <p className="text-center lg:text-center text-md md:text-2xl lg:text-xl">
+              Seems like you’ve landed in the wrong page. Don’t worry, We’ve got
+              you. Let’s take you back to where it’s safe.
+            </p>
+            <div className="py-1">
+              <Link href="/">
+                <p className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full">
+                  Take me back Home
+                </p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default Page;
