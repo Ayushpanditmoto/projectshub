db04eb3ef46046b9d9f494ba2e6bc81215ddd188
Sushmita Biswas <145688707+biswassushmita947@users.noreply.github.com>
2023-09-21 18:33:54 +0000

    fix: aligment not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,21 +10,23 @@ function Page() {
           <Image
             alt="404"
             src="/static/error.png"
-            width={400}
-            height={400}
+            width={430}
+            height={430}
             className="h-90 w-90"
           />
         </div>
         <div className="flex flex-col items-center justify-center">
           <div className="flex-1 max-w-md lg:max-w-xl lg:text-center space-y-3 md:space-y-6">
-            <h1 className="font-bold text-xl md:text-4xl">OPPS! LOST?</h1>
-            <p className="text-center lg:text-center text-md md:text-2xl lg:text-xl">
-              Seems like you’ve landed in the wrong page. Don’t worry, We’ve got
-              you. Let’s take you back to where it’s safe.
+            <h1 className="font-bold text-center text-xl md:text-4xl">
+              Oops! Page not found.
+            </h1>
+            <p className="text-center lg:text-center py-8 text-md md:text-2xl lg:text-xl">
+              Seems like you've landed in the wrong page. Don't worry, We've got
+              you. Let's take you back to where it's safe.
             </p>
             <Link href="/">
               <div className="py-1">
-                <p className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full">
+                <p className="bg-blue-500 text-center hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md">
                   Take me back Home
                 </p>
               </div>
