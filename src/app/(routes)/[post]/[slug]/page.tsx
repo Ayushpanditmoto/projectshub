2ec3887f9215e4b127f7838c161ae41916746979
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-09-22 14:25:29 -0400

    The dynamic routes for the pages that the header redirects were added, in addition another dynamic route was added inside the first one created

---
+++ src/app/(routes)/[post]/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import CenterLayout from '@/app/components/CenterLayout'
+
+const Page = () => {
+  return (
+    <>
+      <CenterLayout>
+        <h1>Page slug</h1>
+      </CenterLayout>
+    </>
+  )
+}
+
+export default Page
