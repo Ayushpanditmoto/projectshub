558b7dc728ff583a1f388d0485a589b8d6a6fe30
Sushmita Biswas <145688707+biswassushmita947@users.noreply.github.com>
2023-09-22 14:45:55 +0530

    feat: added back to blog pages fetures :tada: in single pages

--- src/app/(routes)/blog/[slug]/page.jsx
+++ src/app/(routes)/blog/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import CenterLayout from "@/app/components/CenterLayout";
 import { getPostBySlug } from "@/lib/mdx";
 import { MDXRemote } from "next-mdx-remote";
+import Link from "next/link";
 const getPageContent = async (slug) => {
   const { meta, content } = await getPostBySlug(slug);
   return { meta, content };
@@ -17,6 +18,14 @@ const Page = async ({ params }) => {
   return (
     <CenterLayout>
       <section className="py-5">
+        {/* back to blog */}
+        <Link
+          href="/blog"
+          className="text-sm text-gray-600 hover:text-gray-700"
+        >
+          Back to blog
+        </Link>
+
         <div className="container py-4 prose prose-slate">{content}</div>
       </section>
     </CenterLayout>
